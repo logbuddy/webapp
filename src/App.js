29508cf4c97e0d719a0b40f32bd3ffefb8e609ee
Manuel Kießling <manuel@kiessling.net>
2021-03-13 13:02:09 +0100

    WIP: React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import './App.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
 import RegisterContainer from './containers/RegisterContainer';
 
 function App() {
   return (
-    <div className="App">
-      <RegisterContainer />
-    </div>
+    <Router>
+        <div>
+            <nav className='navbar navbar-expand-sm navbar-light bg-light'>
+                <div className="container-fluid">
+                    <ul className='navbar-nav'>
+                        <li className='nav-item p-2'>
+                            <Link to='/'>Home</Link>
+                        </li>
+                        <li className='nav-item p-2'>
+                            <Link to='/login'>Log in</Link>
+                        </li>
+                        <li className='nav-item p-2'>
+                            <Link to='/register'>Register</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <Switch>
+                <Route path='/'>
+                    <div>Home</div>
+                </Route>
+                <Route path='/login'>
+                    <div>Login</div>
+                </Route>
+                <Route path='/register'>
+                    <RegisterContainer />
+                </Route>
+            </Switch>
+        </div>
+    </Router>
   );
 }
 
