3d9a6077896fde877363af5747af7994f8ebbe29
Manuel Kießling <manuel@kiessling.net>
2021-03-14 17:27:49 +0100

    WIP: App with Redux state

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import {
     BrowserRouter as Router,
     Switch,
@@ -7,46 +9,56 @@ import {
 import RegisterContainer from './containers/RegisterContainer';
 import LoginContainer from './containers/LoginContainer';
 
-function App() {
-  return (
-    <Router>
-        <div>
-            <nav className='navbar navbar-expand-sm navbar-light bg-light'>
-                <div className="container-fluid">
-                    <ul className='navbar-nav'>
-                        <li className='nav-item'>
-                            <NavLink className='nav-link' activeClassName='active' to='/'>
-                                <strong>Home</strong>
-                            </NavLink>
-                        </li>
-                        <li className='nav-item'>
-                            <NavLink className='nav-link' activeClassName='active' to='/login'>Login</NavLink>
-                        </li>
-                        <li className='nav-item'>
-                            <NavLink className='nav-link' activeClassName='active' to='/register'>Register</NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+const mapStateToProps = state => ({
+    reduxState: {...state}
+});
 
-            <Switch>
-                <Route path='/login'>
-                    <LoginContainer />
-                </Route>
-                <Route path='/register'>
-                    <RegisterContainer />
-                </Route>
-                <Route exact path='/'>
-                    <div className='m-4'>
-                        <h1>
-                            Welcome to the GameLoggers PoC.
-                        </h1>
-                    </div>
-                </Route>
-            </Switch>
-        </div>
-    </Router>
-  );
+class App  extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Router>
+                <div>
+                    <nav className='navbar navbar-expand-sm navbar-light bg-light'>
+                        <div className="container-fluid">
+                            <ul className='navbar-nav'>
+                                <li className='nav-item'>
+                                    <NavLink className='nav-link' activeClassName='active' to='/'>
+                                        <strong>Home</strong>
+                                    </NavLink>
+                                </li>
+                                <li className='nav-item'>
+                                    <NavLink className='nav-link' activeClassName='active' to='/login'>Login</NavLink>
+                                </li>
+                                <li className='nav-item'>
+                                    <NavLink className='nav-link' activeClassName='active' to='/register'>Register</NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+
+                    <Switch>
+                        <Route path='/login'>
+                            <LoginContainer />
+                        </Route>
+                        <Route path='/register'>
+                            <RegisterContainer />
+                        </Route>
+                        <Route exact path='/'>
+                            <div className='m-4'>
+                                <h1>
+                                    Welcome to the GameLoggers PoC.
+                                </h1>
+                            </div>
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
 }
 
-export default App;
+export default connect()(App);
