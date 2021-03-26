4a851d4628edbaa8038908ad8c3936fcdf2fdc4b
Manuel Kießling <manuel@kiessling.net>
2021-03-26 09:05:05 +0100

    Reworked the homepage layout massively

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -107,58 +107,75 @@ class AppContainer extends Component {
                             <ServersContainer />
                         </Route>
                         <Route exact path='/'>
-                            <div className='m-0 text-center bg-deepdark'>
-                                <div className='card card-body bg-deepdark p-5'>
-                                    <div className='mb-3'>
+                            <div className='container-fluid'>
+                                <div className='row pb-3 pt-4 navbar-dark'>
+                                    <div className='col-12 text-center'>
                                         <img src='assets/images/logbuddy-logo.png' width='300' alt='LogBuddy Logo'/>
                                         <h4 className='text-white-50'>
                                             Put your logs on the web <span className='text-primary'>in no time.</span>
                                         </h4>
                                     </div>
-                                    <div className='row-cols-lg-2'>
-                                        <div className='card card-body mt-4 p-4 pb-2 bg-white text-dark p-2 ms-auto me-auto'>
-                                            <p className='line-space-2'>
-                                                LogBuddy is at your side whenever you need to quickly <span className='bg-success text-white p-1'>put your log files online</span> in real-time,
-                                                allowing you to <span className='bg-success text-white p-1'>easily share and analyze</span> the output of your applications and services.
-                                            </p>
-                                        </div>
+                                </div>
+                                <div className='row bg-white bg-gradient-transparent-to-bg-secondary'>
+                                    <div className='col-md-6 mt-auto mb-auto'>
+                                        <p className='line-space-2 text-dark ps-5 pe-5 pt-5 pt-lg-0 pt-md-5 pt-sm-5'>
+                                            LogBuddy is at your side whenever you need to quickly <span className='bg-primary text-white p-1'>put&nbsp;your&nbsp;log&nbsp;files&nbsp;online</span> in real-time,
+                                            allowing you to <span className='bg-primary text-white p-1'>easily&nbsp;share&nbsp;and&nbsp;analyze</span> the output of your applications and services.
+                                        </p>
                                     </div>
+                                    <div className='col-md-6 text-end me-0 pe-0 mb-0 pb-0 mt-md-5'>
+                                        <img src='assets/images/logbuddy-cli-to-browser-demo.png' className='mw-100' width='90%' alt='LogBuddy CLI to Browser Demo'/>
+                                    </div>
+                                </div>
 
-                                    <div className='row-cols-lg-2'>
-                                        <div className='card card-body mt-4 p-4 pb-2 bg-secondary p-2 ms-auto me-auto'>
-                                            <p>
-                                                Start now by setting up your first server:
-                                            </p>
-                                            <p className='bg-dark rounded p-1 pt-0'>
-                                                <code className='tiny text-success'>
-                                                      bash -c "$(curl -fsSL https://raw.githubusercontent.com/logbuddy/bash-clients/main/src/sl-install)"
-                                                </code>
-                                            </p>
-                                            <p>
-                                                No need to register here first - the above CLI command takes care of that.
-                                            </p>
-                                            <p>
-                                                Running the above command on your server will guide you through setting
-                                                up your LogBuddy account and will enable your server to send whatever log file
-                                                output you like into that account, making your log file content available on
-                                                the web in real-time.
-                                            </p>
-                                            <p>
-                                                The setup script doesn't need any kind of root access on your server, and
-                                                you can always check out its source code
-                                                at <a className='text-white' href='https://github.com/logbuddy/bash-clients/tree/main/src'>GitHub.com</a>.
-                                            </p>
-                                            <p>
-                                                Here's how a CLI setup session actually looks like (click to enlarge):
-                                            </p>
-                                            <p>
-                                                <a href='/assets/images/logbuddy-cli-session-full.png'>
-                                                    <img src='/assets/images/logbuddy-cli-session-small.png' className='w-100' alt='LogBuddy Logo'/>
-                                                </a>
-                                            </p>
+                                <div className='row bg-secondary ps-2 pe-2 pt-5 pb-4'>
+                                    <div className='col-lg-6 pe-md-5 pb-2 ps-5 pe-5'>
+                                        <p>
+                                            Start now by setting up your first server:
+                                        </p>
+                                        <p className='bg-dark rounded p-3 pt-1 pb-2'>
+                                            <code className='tiny text-info'>
+                                                bash -c "$(curl -fsSL https://raw.githubusercontent.com/logbuddy/bash-clients/main/src/sl-install)"
+                                            </code>
+                                        </p>
+                                        <p>
+                                            No need to register on the web first - the above CLI command takes care of that.
+                                        </p>
+                                    </div>
+                                    <div className='col-lg-6 text-center ps-md-5 ps-5 pe-5'>
+                                        <div className='bg-dark rounded p-3 pt-1 pb-2'>
+                                            <div className='mt-3 mb-3'>
+                                                Here is what happens in the CLI
+                                            </div>
+                                            <a href='/assets/images/logbuddy-cli-session-full.png'>
+                                                <img src='/assets/images/logbuddy-cli-session-small.png' className='w-50' alt='LogBuddy Logo'/>
+                                            </a>
+                                            <div className='small mt-3 mb-3 text-secondary'>
+                                                Click to enlarge
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+
+                                <div className='row bg-dark ps-2 pe-2 pt-4 pb-5'>
+                                    <div className='col-md-6 pe-md-5 pb-5 ps-5 pe-5'>
+                                        <p>
+                                            Running the above command on your server will guide you through setting
+                                            up your LogBuddy account and will enable your server to send whatever log file
+                                            output you like into that account, making your log file content available on
+                                            the web in real-time.
+                                        </p>
+                                        <p>
+                                            The setup script doesn't need any kind of root access on your server, and
+                                            you can always check out its source code
+                                            at <a className='text-white' href='https://github.com/logbuddy/bash-clients/tree/main/src'>GitHub.com</a>.
+                                        </p>
+                                    </div>
+                                    <div className='col-md-6 text-center ps-md-5'>
+                                        &nbsp;
+                                    </div>
+                                </div>
+
                             </div>
                         </Route>
                     </Switch>
