7bf0cfab60b31897c3b9829a5ce30ee181124a65
Manuel Kießling <manuel@kiessling.net>
2021-04-03 17:54:00 +0200

    Layouting

--- src/presentationals/eventSkins/SkinBox.js
+++ src/presentationals/eventSkins/SkinBox.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SkinBox = ({type, logo, children}) => (
-    <div className={`mt-2 mb-2 alert alert-${type}`}>
+    <div className={`mt-0 mb-2 alert alert-${type}`}>
         <div className='ms-2 float-end'>{logo}</div>
         {children}
     </div>
