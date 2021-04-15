94c2dffa80b76081a724a8afc199600c86214690
Manuel Kießling <manuel@kiessling.net>
2021-04-15 18:05:24 +0200

    WIP: preprod infrastructure

--- infrastructure/terraform/bootstrap/variables.tf
+++ infrastructure/terraform/bootstrap/variables.tf
@@ -10,7 +10,7 @@ variable "workspace_to_infra_webapp_aws_account_id" {
   type = map(string)
   default = {
     default = "230024871185"
-    preprod = "000000000000"
+    preprod = "619527075300"
   }
 }
 
@@ -18,6 +18,6 @@ variable "workspace_to_root_domain" {
   type = map(string)
   default = {
     default = "app.logbuddy.io"
-    preprod = ""
+    preprod = "preprod.app.logbuddy.io"
   }
 }
