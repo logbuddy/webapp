3b22cf3b07fa31c7847d388121aa7914f490ae8e
Manuel Kießling <manuel@kiessling.net>
2021-04-19 09:45:43 +0200

    Bash script improvements

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -10,27 +10,27 @@ AWS_ACCOUNT_ID="619527075300"
 [ -x /opt/homebrew/bin/brew ] && [ -s "$(/opt/homebrew/bin/brew --prefix)/opt/nvm/nvm.sh" ] && source "$(/opt/homebrew/bin/brew --prefix)/opt/nvm/nvm.sh"
 
 
-pushd "$DIR/../backend/rest-api/default"
+pushd "$DIR/../backend/rest-api/default" || exit
   rm -rf build
   nvm install
   nvm use
   npm i --no-save
   npm run build
   cp -a node_modules build/
-  pushd build
+  pushd build || exit
     zip -r rest_api_default.zip ./
     source "$DIR/../../infrastructure-bootstrap/bin/assume-role.sh" "$AWS_ACCOUNT_ID"
     aws s3 cp ./rest_api_default.zip "s3://herodot-infra-webapp-$STAGE-rest-api-lambdas/default/$DEPLOYMENT_NUMBER/rest_api_default.zip"
     source "$DIR/../../infrastructure-bootstrap/bin/unassume-role.sh"
-  popd
-popd
+  popd || exit
+popd || exit
 
-pushd "$DIR/../infrastructure/terraform/main"
+pushd "$DIR/../infrastructure/terraform/main" || exit
   terraform-0.15.0 workspace select "$STAGE"
   terraform-0.15.0 apply -var deployment_number="$DEPLOYMENT_NUMBER"
-popd
+popd || exit
 
-pushd "$DIR/../frontend"
+pushd "$DIR/../frontend" || exit
   rm -rf build
   nvm install
   nvm use
@@ -39,4 +39,4 @@ pushd "$DIR/../frontend"
   source "$DIR/../../infrastructure-bootstrap/bin/assume-role.sh" "$AWS_ACCOUNT_ID"
   aws s3 cp --recursive --acl public-read build/ "s3://herodot-infra-webapp-$STAGE-frontend/"
   source "$DIR/../../infrastructure-bootstrap/bin/unassume-role.sh"
-popd
+popd || exit
