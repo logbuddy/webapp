9ece8c1c781a2f9642799987f1a344afb3a7b33c
Manuel Kießling <manuel@kiessling.net>
2021-05-29 08:47:55 +0200

    The create-react-app dev server now proxies unknown requests to the preprod

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,8 +2,4 @@ const config = {
     backendApiBaseUri: 'api',
 };
 
-if (process.env.hasOwnProperty('NODE_ENV') && process.env.NODE_ENV === 'development') {
-    config.backendApiBaseUri = 'https://preprod.app.logbuddy.io/api';
-}
-
 export default config;
